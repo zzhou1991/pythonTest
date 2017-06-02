2a2f7d75b6e74088bf707af5f67f5ba169920c6a
zzhou1991sz <3zhouzhen@163.com>
2017-06-02 18:27:38 +0800

    test.py

--- test.py
+++ test.py
@@ -1,10 +1,5 @@
 def normalize(name):
-    def lowerC(ch):
-        if not ch.islower():
-            ch.lower()
-        return ch
-    
-    return list(map(lowerC, name))
+		if 
 
 L1 = ['adam', 'LISA', 'barT']
 L2 = list(map(normalize, L1))
